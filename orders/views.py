6cb4aa12b7b9af368d7facefe8661cedf050618a
paridhimaheshwari <priti.paridhi@gmail.com>
2023-07-08 02:35:30 +0530

    made sandbox accounts, and successful payments from individual paypal

--- orders/views.py
+++ orders/views.py
@@ -5,9 +5,28 @@ from django.shortcuts import redirect
 from .forms import OrderForm
 from .models import Order, OrderProduct, Payment
 import datetime
+import json
 
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user = request.user, is_ordered = False, order_number = body['orderID'])
+
+    
+    # Store transaction details inside payment model
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
     return render(request,'orders/payments.html')
 
 
